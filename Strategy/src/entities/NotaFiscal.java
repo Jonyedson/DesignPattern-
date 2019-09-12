bb38c5e6e669693809fb019a3b238aa26d937d89
Cicero Jonyedson <cicerojonyedison@gmail.com>
2019-09-12 03:10:43 -0300

    Classe notafiscal

---
+++ Strategy/src/entities/NotaFiscal.java
@@ -0,0 +1,23 @@
+package entities;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotaFiscal {
+    List<Produto> product = new ArrayList<>();
+
+    public List<Produto> getProduct() {
+        return product;
+    }
+
+    public void addProcduct(Produto products){
+        product.add(products);
+    }
+
+    public void RemoveProcduct(Produto products){
+        product.remove(products);
+    }
+    
+    
+}
