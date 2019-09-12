602744fd624779c0621e83fe836576f2d851c8e4
Cicero Jonyedson <cicerojonyedison@gmail.com>
2019-09-12 02:17:22 -0300

    Implementando classes livros

---
+++ Strategy/src/entities/LivroDigital.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class LivroDigital extends Livro {
+
+    @Override
+    public void Pagamento() {
+        
+    }
+    
+}
---
+++ Strategy/src/entities/LivroImpresso.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class LivroImpresso extends Livro {
+
+    @Override
+    public void Pagamento() {
+        
+    }
+
+}
