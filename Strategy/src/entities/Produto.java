f345028e05c375a6e60a0a97db8e3e7b1cc99314
Cicero Jonyedson <cicerojonyedison@gmail.com>
2019-09-13 19:23:52 -0300

    Ajustes

--- Strategy/src/entities/Produto.java
+++ Strategy/src/entities/Produto.java
@@ -5,10 +5,7 @@ public abstract class Produto {
     protected Double price;
     protected Desconto desconto;
 
-    public Desconto getDesconto() {
-        return desconto;
-    }
-
+    
     public void setDesconto(Desconto desconto) {
         this.desconto = desconto;
     }
